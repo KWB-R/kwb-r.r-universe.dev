4f194eb6b62d21217df1fa2341dc396076c8e049
mrustl <michael.rustler@kompetenz-wasser.de>
2021-07-13 13:40:21 +0200

    Remove "universe" config

--- universe.R
+++ universe.R
@@ -1,3 +1,4 @@
-remotes::install_github("kwb-r/pkgmeta@dev")
+#remotes::install_github("kwb-r/pkgmeta")
+install.packages("pkgmeta", repos = "https://kwb-r.r-universe.dev/")
 writeLines(pkgmeta::create_universe_pkgs_json(github_token = ""),
            "packages.json")
