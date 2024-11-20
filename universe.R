4312133536d5f7b7731fc3e6acb7996a4005f752
mrustl <michael.rustler@kompetenz-wasser.de>
2024-11-20 10:58:02 +0100

    Add new R packages (since 2022)

--- universe.R
+++ universe.R
@@ -1,4 +1,6 @@
 #remotes::install_github("kwb-r/pkgmeta")
 install.packages("pkgmeta", repos = "https://kwb-r.r-universe.dev/")
+remotes::install_github("kwb-r/pkgmeta")
+remotes::install_github("kwb-r/kwb.pkgstatus@dev")
 writeLines(pkgmeta::create_universe_pkgs_json(github_token = ""),
            "packages.json")
